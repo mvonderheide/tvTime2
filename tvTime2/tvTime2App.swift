b14214b98ba1de3b1661cbafa030cf2998d4ac94
Matthew Vonderheide <msvonderheid@cps.edu>
2022-05-04 12:25:54 -0500

    Initial Commit

---
+++ tvTime2/tvTime2App.swift
@@ -0,0 +1,17 @@
+//
+//  tvTime2App.swift
+//  tvTime2
+//
+//  Created by Administrator on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct tvTime2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
